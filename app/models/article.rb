f8581bfc3c09e42246a6577665c7120ce14fa4be
Gautam Srinivasan <gautam.srinivasan7@gmail.com>
2015-04-16 00:23:10 -0700

    Initial Commit

---
+++ app/models/article.rb
@@ -0,0 +1,57 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  text       :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'elasticsearch/model'
+
+class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :text, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            minimum_should_match: ['20%'],
+            fields: ['title^10', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
+end
+
+# Delete the previous articles index in Elasticsearch
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+
+# Create the new index with the new mapping
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+
+# Index all article records from the DB to Elasticsearch
+Article.import
